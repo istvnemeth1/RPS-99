3e2802f7c4c2f80d64d084ec1b06d38edd8812e0
Istvan Nemeth <istvnemeth1@gmail.com>
2021-02-11 11:24:40 +0000

    Update game-logic.js
    Somme main functions
    Installed mocha and chai

--- js/game-logic.js
+++ js/game-logic.js
@@ -13,9 +13,33 @@ let playerTwoMoveTwoValue;
 let playerTwoMoveThreeValue;
 
 const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+    if (!moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeType || !moveThreeValue) {
+        return;
+    }
+
+    if (!isValidMoveType(moveOneType) || !isValidMoveType(moveTwoType) || !isValidMoveType(moveThreeType)) {
+        return;
+    }
+
+    if (!isValidMoveValue(moveOneValue) || !isValidMoveValue(moveTwoValue) || !isValidMoveValue(moveThreeValue)) {
+        return;
+    }
+
+    if ((moveOneValue + moveTwoValue + moveThreeValue) > 99) {
+        return;
+    }
+
+    // Helper function
+
 
 }
 
+
+
 const getRoundWinner = () => {
     
+}
+
+const getGameWinner = () => {
+
 }
